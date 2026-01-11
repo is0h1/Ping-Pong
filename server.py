dd01ee705cf2731ffcb81fd2554c186c8d1bbbcc
is0h1 <anaksymchuk2@gmail.com>
2026-01-11 13:26:22 +0200

    Update server.py

--- server.py
+++ server.py
@@ -87,3 +87,76 @@ class GameServer:
                     self.connected[pid] = False
                     
     def ball_logic (self):
+        while self.countdown > 0:
+            time.sleep(1)
+            with self.lock:
+                self.countdown -= 1
+                self.broadcast_state()
+
+        while not self.game_over:
+            with self.lock:
+                self.ball['x'] += self.ball['vx']
+                self.ball['y'] += self.ball['vy']
+
+                if self.ball['y'] <= 60 or self.ball['y'] >= HEIGHT:
+                    self.ball['vy'] *= -1
+                    self.sound_event = "wall_hit"
+
+                if (self.ball['x'] <= 40 and self.paddles[0] <= self.ball['y'] <= self.paddles[0] + 100) or \
+                    (self.ball['x'] >= WIDTH - 40 and self.paddles[1] <= self.ball['y'] <= self.paddles[1] + 100):
+                    self.ball['vx'] *= -1
+                    self.sound_event = 'platform_hit'
+
+                if self.ball['x'] < 0:
+                    self.score[1] += 1
+                    self.reset_ball()
+                elif self.ball['x'] > WIDTH:
+                    self.score[0] += 1
+                    self.reset_ball()
+
+                if self.score[0] >= 10:
+                    self.game_over = True
+                    self.winner = 0
+                elif self.scores[1] >= 10:
+                    self.game_over = True
+                    self.winner = 1
+
+                self.broadcast_state()
+                self.sound_event = None
+            time.sleep(0.016)
+
+    def reset_ball(self):
+        self.ball = {
+            "x": WIDTH // 2,
+            "y": HEIGHT // 2,
+            "vx": BALL_SPEED * random.choice([-1, 1]),
+            "vy": BALL_SPEED * random.choice([-1, 1])
+        }
+
+    def accept_player(self):
+
+        print("Очікуємо гравця 0...")
+        conn, _ = self.server.accept()
+        self.clients[0] = conn
+        conn.sendall(b"0\n")
+        self.connected[0] = True
+        print("Гравець 0 приєднався")
+        threading.Thread(target=self.handle_client, agrs=(0,), daemon=True).start()
+
+
+        print("Очікуємо гравця 1 (5 секунд)...")
+        self.server.settimeout(5)
+        try:
+            conn, _ = self.server.accept()
+            self.clients[1] = conn
+            conn.sendall(b"1\n")
+            self.connected[1] = True
+            print("Гравець 1 приєднався")
+            threading.Thread(target=self.handle_client, agrs=(1, ), daemon=True).start()
+        except socket.timeout:
+            print("Другий гравець не підключився. Активую бота.")
+            self.bot_active = True
+            self.connected[1] = True
+            threading.Thread(target=self.run_bot, daemon=True).start()
+        finally:
+            self.server.settimeout(None)
